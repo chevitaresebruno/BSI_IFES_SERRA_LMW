8d0ad581a5e7661faa5fd43834b5abb09d49022c
Bruno <chevitarese.bruno@gmail.com>
2024-03-24 19:15:17 -0300

    Adição de algumas estruturas e salvar para eu ir amimir

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('autor/<int:pk>/', views.author_projects, name='author_projects'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Author, Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    autores = Author.objects.all()
+    return render(request, 'blog/home.html', {'autores': autores})
+
+
+def author_projects(request, pk):
+    posts = Post.objects.filter(author__id__contains=pk)
+    return render(request, 'blog/posts/author_project.html', {'posts': posts})
